e507f6c565763f75c10d91d04e17fbd0cd24d255
gautam1202 <gautam1202@users.noreply.github.com>
2020-04-14 16:47:24 +0530

    SpringMVC

---
+++ SpringMVCJpaLab2/src/main/java/com/cg/SpringMvcJpaLab2Application.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcJpaLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcJpaLab2Application.class, args);
+	}
+
+}
